6d98c9585c049a36d136ddab7dca1ae457fae95f
Sergio Beaumont <xx830@globalia.com>
2023-08-22 14:57:51 +0100

    solved issue about validation

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -12,4 +12,10 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
       secretOrKey: config.get('JWT_SECRET'),
     });
   }
+
+  validate(payload: any) {
+    console.log(payload);
+
+    return payload;
+  }
 }
